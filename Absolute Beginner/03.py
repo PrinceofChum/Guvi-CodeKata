f84de6fd7e5aa39b90e4d02c157c52034bac5c54
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2020-12-21 20:05:00 +0530

    print the largest among the three
    You are given three numbers A, B & C. Print the largest amongst these three numbers.
    
    Input Description:
    Three numbers are provided to you.
    
    Output Description:
    Find and print the largest among the three
    
    Sample Input :
    1
    2
    3
    Sample Output :
    3

---
+++ Absolute Beginner/03.py
@@ -0,0 +1,16 @@
+#for a fun, made it in a detailed way.
+
+result=list()
+
+A= int(input())
+
+B=int(input())
+
+C=int(input())
+
+result.append(A)
+result.append(B)
+result.append(C)
+
+
+print(max(result))
